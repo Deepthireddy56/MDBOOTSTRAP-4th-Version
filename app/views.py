0ef45d14ab7ba67a302e8c557f4b4496be484c71
Deepthireddy56 <bobbadeepthi86@gmail.com>
2023-08-14 23:10:35 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdboot(request):
+    return render(request,'mdboot.html')
+
+def mdboot1(request):
+    return render(request,'mdboot1.html')
+
+def mdboot2(request):
+    return render(request,'mdboot2.html')
+
+def mdboot3(request):
+    return render(request,'mdboot3.html')
+
+def mdboot4(request):
+    return render(request,'mdboot4.html')
+
+def mdboot5(request):
+    return render(request,'mdboot5.html')
+
+def mdboot6(request):
+    return render(request,'mdboot6.html')
